30495085f40b59615f0fcb43a2695b03e50df133
abhijitkneo <abhijit.kangralkar@neosofttech.com>
2026-06-08 11:58:06 +0530

    Implemented product details page

---
+++ src/app/products/[id]/[slug]/page.tsx
@@ -0,0 +1,45 @@
+import ProductRatings from "@/components/common/ProductRatings";
+import { getProductById } from "@/services/ProductService";
+import { Button, Col, Row } from "react-bootstrap";
+import { IoIosStar } from "react-icons/io";
+
+type Props = {
+	params: Promise<{
+		slug: string,
+		id: string
+	}>
+}
+
+export default async function ProductDetailsPage({params}: Props) {
+	const {id} = await params;
+	const product = await getProductById(id);
+	console.log(product, '+++ single product');
+	
+
+	return (
+		<section className="my-3">
+			<Row>
+				<Col md={3}>
+					<div className="bg-white rounded-3 p-3 border border-dark border-opacity-10">
+						<figure className="m-0 bg-dark bg-opacity-10 rounded-3 p-3">
+							<img src={product.image} alt="" className="img-fluid" />
+						</figure>
+					</div>
+				</Col>
+				<Col md={9}>
+					<div className="bg-white rounded-3 p-3 border border-dark border-opacity-10 h-100">
+						<span className="d-inline-block rounded-2 bg-primary-subtle text-uppercase small fw-semibold px-2 mb-2">{product.category}</span>
+						<h1 className="fs-5 mb-1">{product.title}</h1>
+						<div className="mb-3">
+							<ProductRatings rate={product.rating.rate} count={product.rating.count} />
+						</div>
+						<hr />
+						<p>{product.description}</p>
+						<h2 className="fw-semibold mb-3">${product.price.toFixed(2)}</h2>
+						<Button type="button" variant="primary">Add to Cart</Button>
+					</div>
+				</Col>
+			</Row>
+		</section>
+	)
+}
--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -1,8 +1,11 @@
 'use client';
 import { Product } from '@/types'
+import { slugify } from '@/utils/slugify';
+import Link from 'next/link';
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import { BsStarFill } from 'react-icons/bs'
+import { LuExternalLink } from 'react-icons/lu';
 
 interface ProductCardProps {
 	product: Product
@@ -13,9 +16,14 @@ const ProductCard = ({ product }: ProductCardProps) => {
 		<>
 		<Card className='product-card'>
 			<Card.Body>
-				<figure className='text-center bg-dark bg-opacity-10 p-3 rounded-2 flex-center'>
-					<img src={product.image} alt="" className='img-fluid' />
-				</figure>
+				<Link href={`/products/${product.id}/${slugify(product.title)}`} className='product-img-link'>
+					<figure className='text-center bg-dark bg-opacity-10 p-3 rounded-2 flex-center overflow-hidden position-relative'>
+						<img src={product.image} alt="" className='img-fluid' />
+						<span className='d-block bg-primary position-absolute rounded p-2 link-icon shadow'>
+							<LuExternalLink size={22} className='text-white lh-1' />
+						</span>
+					</figure>
+				</Link>
 				<div className="d-flex align-items-center justify-content-between mb-2">
 					<Card.Text className='text-capitalize text-primary small mb-0 opacity-75'>{product.category}</Card.Text>
 					<p className="m-0 small d-inline-flex align-items-center gap-2 lh-1">
